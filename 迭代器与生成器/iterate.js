0d9b24469b63b2faba5f09d2a956a065e25de4c5
Chances Cheng <cyqforwork@163.com>
2024-05-07 22:33:30 +0800

    Create iterate.js
    迭代器和生成器

---
+++ 迭代器与生成器/iterate.js
@@ -0,0 +1,9 @@
+let collection=['foo','bar','baz']
+for (let i = 0; i < collection.length; i++) {
+    console.log(collection[i])
+}
+let collection_01=['1','2','3']
+collection_01.forEach((item)=>{
+    console.log(item)
+})
+//迭代器模式
