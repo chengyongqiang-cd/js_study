60df03c0b275bec79c9fcd360deeec8f84b1ccd3
Chances Cheng <cyqforwork@163.com>
2024-05-08 23:01:47 +0800

    Update test_object.js
    对象和类的学习，学习到第251页

--- 对象、类与面向对象编程/test_object.js
+++ 对象、类与面向对象编程/test_object.js
@@ -36,3 +36,75 @@ let book={
     year_:2017,
     edition:1
 }
+Object.defineProperties(book,{
+    year:{
+        get(){
+            return this.year_
+        },
+        set(value){
+            if(value>2017){
+                this.year_=value
+                this.edition+=value-2017
+            }
+        }
+    }
+})
+book.year=2018
+console.log(book.edition)
+
+console.log(Object.getOwnPropertyDescriptors(book))
+
+//解构
+let car={
+    name:"bmw",
+    age:3
+}
+let {name,age,color='black'}=car
+console.log(name,age,color)
+
+let {length}='doitrrrr'
+console.log(length)
+
+
+function createPerson(name,age,job){
+    let o=new Object()
+    o.name=name
+    o.age=age
+    o.job=job
+    o.sayName=function (){
+        console.log(this.name)
+    }
+    return o
+}
+
+let person1=createPerson('chengyongqiang',18,'codeman')
+person1.sayName()
+
+function Person(name,age,job){
+    this.name=name
+    this.age=age
+    this.job=job
+    this.sayname=function (){
+        console.log(this.name)
+    }
+}
+let person2=new Person('wenjin',19,'freedomwomem')
+person2.sayname()
+//函数表达式定义构造函数
+let Car=function (name,age,color){
+    this.name=name
+    this.age=age
+    this.color=color
+    this.discription=function (){
+        console.log(this.name+this.age+this.color)
+    }
+}
+let car1=new Car('BMW',3,'black')
+car1.discription()
+
+//类
+class Person{
+
+}
+
+const Animal=class {}
